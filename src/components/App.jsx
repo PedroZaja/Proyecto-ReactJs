777e55b30a8402b8e47df647f85ccf8206970294
PedroZaja <pedrodavid21@gmail.com>
2022-11-21 20:19:09 -0300

    proyecto react

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import ItemList from './ItemList/ItemList';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <ItemList/>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,9 @@
+const ItemList = ({greeting}) => {
+    return (
+        <div>
+            <p>{greeting}</p>
+        </div>
+    );
+}
+
+export default ItemList;
